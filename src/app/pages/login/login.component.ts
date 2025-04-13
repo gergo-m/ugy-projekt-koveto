1a1462234837a7bdf0da5d286a3d030566f5c485
Mindszenti Gergő <mindszenti.gergo@gmail.com>
2025-04-13 15:03:53 +0200

    finish login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -23,34 +23,46 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  email = new FormControl('');
-  password = new FormControl('');
-  isLoading: boolean = false;
-  loginError: string = '';
-  showLoginForm: boolean = true;
-
   loginForm = new FormGroup({
-    email: this.email,
-    password: this.password
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
   });
+
+  isLoading: boolean = false;
+  showForm: boolean = true;
+  loginError: string = '';
 
   constructor(private router: Router) {}
 
-  login() {
-    this.loginError = '';
-    
-    if (this.email.value === 'test@gmail.com' && this.password.value === 'testpw') {
-      console.log('Login function called!');
-      this.isLoading = true;
-      this.showLoginForm = false;
-      
-      localStorage.setItem('isLoggedIn', 'true');
-      
-      setTimeout(() => {
-        this.router.navigate(['/dashboard']);
-      }, 3000);
-    } else {
-      this.loginError = 'Invalid email or password!';
+  login(): void {    
+    if (this.loginForm.invalid) {
+      this.loginError = "Please correct the form errors before submitting.";
+      return;
     }
+
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+
+    if (email !== 'test@example.com' || password !== 'testpw123') {
+      this.loginError = 'Invalid email or password!';
+      return;
+    }
+
+    this.isLoading = true;
+    this.showForm = false;
+    
+    localStorage.setItem('isLoggedIn', 'true');
+    
+    setTimeout(() => {
+      this.router.navigate(['/dashboard']);
+    }, 3000);
+  }
+
+  hasErrors(formControl: string) {
+    return this.loginForm.get(formControl)?.invalid && this.loginForm.get(formControl)?.touched && !this.loginForm.get(formControl)?.errors?.["required"];
+  }
+
+  getErrors(formControl: string) {
+    return this.loginForm.get(formControl)?.errors;
   }
 }
