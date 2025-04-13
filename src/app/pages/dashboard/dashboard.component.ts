b30b701600b2a44b172546dda985740ff0b624f2
Mindszenti Gergő <mindszenti.gergo@gmail.com>
2025-04-13 19:36:57 +0200

    finish dashboard component

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,11 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterLink } from '@angular/router';
+import { ProfileObject } from '../../shared/constant';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     MatButtonModule,
+    MatCardModule,
+    MatIconModule,
     RouterLink,
     CommonModule
   ],
@@ -14,6 +19,21 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  user = ProfileObject[0];
+  currentYear = new Date().getFullYear();
+
+  getWelcomeMessage(): string {
+    const currentHour = new Date().getHours();
+    if (currentHour < 12) {
+      return 'Good Morning';
+    } else if (currentHour < 18) {
+      return 'Good Afternoon';
+    } else {
+      return 'Good Evening';
+    }
+  }
+
   isLoggedIn = false;
 
   constructor(private router: Router) {}
