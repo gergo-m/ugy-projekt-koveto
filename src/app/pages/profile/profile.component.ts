55f35416dfa1a101452cb51d8d2de972679b1727
Mindszenti Gergő <mindszenti.gergo@gmail.com>
2025-04-13 17:04:51 +0200

    finish profile component

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileObject } from '../../shared/constant';
+import { User } from '../../shared/models/User';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -23,9 +24,25 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent implements OnInit {
-  ProfileObject = ProfileObject;
-  
+  ProfileObject: User[] = ProfileObject;  
   selectedIndex: number = 0;
+
+  getFullName(): string {
+    const user = this.ProfileObject[this.selectedIndex];
+    return `${user.name.first_name} ${user.name.last_name}`;
+  }
+
+  getUserRole(): string {
+    return this.ProfileObject[this.selectedIndex].account.role;
+  }
+
+  getCompletionRate(): string {
+    const tasks = this.ProfileObject[this.selectedIndex].statistics.tasks;
+    if (tasks.assigned === 0) {
+      return '0.00';
+    }
+    return ((tasks.completed / tasks.assigned) * 100).toFixed(2);
+  }
 
   ngOnInit(): void {
     this.selectedIndex = 0;
